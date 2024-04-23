8a6814b1d9e6d90976893f6083ee5d42fc0529fb
alannatomaz <160501486+alannatomaz@users.noreply.github.com>
2024-04-23 17:37:53 -0300

    Create sequencia_par_impar_dobro_triplo.c

---
+++ sequencia_par_impar_dobro_triplo.c
@@ -0,0 +1,50 @@
+//CABECALHO
+//Autor: Alanna Tomaz de Aquino Martins 
+//Matricula: 2412130055
+//Objetivo: USO DO WHILE
+//Data: 23/04/2024
+
+#include <stdio.h>
+#include <stdlib.h>
+
+  int main (void){
+    int numero = 0;
+    int dobro = 0;
+    int triplo = 0;
+    int i = 0;
+
+    printf("Insira um numero:");
+    scanf("%d", &numero);
+    
+
+    if(numero%2 == 0){
+        printf("-ok, voce inseriu um numero PAR!");
+        dobro = (numero * 2);
+        printf("\n-O dobro do número inserido é: %d", dobro);
+        
+        printf("\n-A sequência dos 10 proximos numeros a partir do dobro do numero inserido e: ");
+            
+            while(i < 10){
+                i = i + 1;
+                printf("\n %d", dobro + i);
+                
+            }
+    }else if(numero%2 == 1){
+        printf("-ok, voce inseriu um numero IMPAR!");
+        triplo = (numero * 3);
+        printf("\n-O triplo do número inserido é: %d", triplo);
+        
+        printf("\n-A sequência dos 10 proximos numeros a partir do triplo do numero inserido e: ");
+            
+            while(i < 10){
+                i = i + 1;
+                printf("\n %d", triplo + i);
+            }
+            
+    }
+    
+
+  return 0;
+
+  }
+
