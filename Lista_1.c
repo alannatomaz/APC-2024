8fc866a18b384cb62549cc657369af01d94051d4
alannatomaz <160501486+alannatomaz@users.noreply.github.com>
2024-04-30 23:48:26 -0300

    Update Lista_1.c

--- Lista_1.c
+++ Lista_1.c
@@ -1,5 +1,6 @@
 //Alanna
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (void){
     int numero_usuario = 0;
@@ -23,23 +24,80 @@ int main (void){
     return 0;
 } 
 
-    
-    
-    //Lucas
+-----------------------------------------------------------------------------------
+//QUESTAO 2: Receba do usuário via console uma sequencia de números inteiros até que 5 números ímpares sejam lidos.
+//          -Imprima o primeiro número ímpar lido.
+//          -Imprima o último número ímpar lido.
+//          -Imprima quantas vezes o laço precisou ser repetido.    
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    int numero_lido = 0, numero_repeticoes = 0, numeros_pares = 0;
-    printf("Meu bem, este codigo lera numeros inteiros ate que 3 numeros pares sejam lidos.");
-    while (numeros_pares < 3) {
-        printf("\nDigite um numero inteiro: ");
-        scanf("%d", &numero_lido); 
-        numero_repeticoes++;
-        if (numero_lido % 2 == 0) numeros_pares++;
-        printf("O while rodou %dx, ate o momento leu %d numeros pares e nesta iteracao leu o numero %d", numero_repeticoes, numeros_pares, numero_lido);
-        
-    }
-    printf("\nFim");
-    return 0;
+int main (){
+	int numero_usuario = 0;
+	int numeros_impares = 0;
+	int quantidade_repeticoes = 0;
+	int primeiro_impar = 0;
+	int ultimo_impar = 0;
+	
+	printf("Insira uma sequencia de numeros inteiros:\n");
+	
+	while(numeros_impares < 5){
+				scanf("%d", &numero_usuario);
+				quantidade_repeticoes++;
+		
+		if(numero_usuario %2 == 1){
+			numeros_impares++;
+			
+			if(numeros_impares == 1){
+				primeiro_impar = numero_usuario;
+			}
+			ultimo_impar = numero_usuario;
+		}
+				
+	}
+	
+	printf("\n-O primeiro numero impar lido foi: %d", primeiro_impar);
+	printf("\n-O ultimo numero impar lido foi: %d", ultimo_impar);
+	printf("\n-O loop precisou ser repetido %dx ate que tivesse 5 numeros impares.", quantidade_repeticoes);
+	
+	
+	return 0;	
+	
 }
+    
+---------------------------------------------------------------------------------------------
+
+//QUESTAO 3: Receba do usuário via console as variáveis m,x e n do tipo float e calcule o valor de y:
+//          -Equaçao reduzida da reta é y: mx + n
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	float m;
+	float x;
+	float n;
+	float y;
+	
+	
+	printf("Digite um valor para o coeficiente ANGULAR m: ");
+	scanf("%f", &m);
+
+	printf("Digite um valor para o coeficiente LINEAR n: ");
+	scanf("%f", &n);
+	
+	printf("Digite um valor para a variavel x: ");
+	scanf("%f", &x);	
+	
+	y = m * x + n;
+	
+	printf("\nNesse caso o valor de y fica: %.1f", y);
+	
+	
+	return 0;
+}
+
+--------------------------------------------------------------------------------------------
+    
+  
