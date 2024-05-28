004e2dbdf1177fd79ab454ae3545f544f9fc2cea
alannatomaz <160501486+alannatomaz@users.noreply.github.com>
2024-05-28 16:51:16 -0300

    Update array.c

--- array.c
+++ array.c
@@ -14,3 +14,25 @@
 	system("pause");
 	return 0;
 }
+
+----------------------------------------------------------------------
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	int valores[6];
+	int i = 0;
+
+	for(i; i < 6; i++){
+		printf("Digite o valor %d: ", i +1);
+		scanf("%d", &valores[i]);
+	}
+	printf("Os valores lidos foram: ");
+	for(i = 0; i < 6; i++){
+		printf(" %d", valores[i]);
+		
+	}
+	
+
+}
