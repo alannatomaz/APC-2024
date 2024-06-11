7e33c29ab261f1d600229e3cf84c6bbdb0a863f2
alannatomaz <160501486+alannatomaz@users.noreply.github.com>
2024-06-11 09:15:22 -0300

    Update Lista5.c

--- Lista5.c
+++ Lista5.c
@@ -5,9 +5,11 @@ include <stdio.h>
 int main() {
     char string[11];
     int i;
+    
     printf("Digite uma string de ate 10 caracteres: \n");
     gets(string);
     printf("Os primeiros quatro caracteres sao: \n");
+    
     for (i = 0; i < 4; i++) {
         printf("%c\n", string[i]);
     }
@@ -22,8 +24,10 @@ int main() {
     char string[51];
     int i;
     int quantidade_caracteres_string = 0;
+    
     printf("Digite uma string de ate 50 caracteres: \n");
     gets(string);
+    
     for (i = 0; string[i] != '\0'; i++) {
         quantidade_caracteres_string++;
     }
@@ -42,10 +46,14 @@ int main() {
     int i;
     int tamanho_string;
     int posicao_caractere_final;
+    
     printf("Digite uma string de ate 10 caracteres a ser imprimida de tras pra frente: \n");
     gets(string);
+    
     tamanho_string = strlen(string);
+    
     posicao_caractere_final = tamanho_string - 1;
+    
     for (i = posicao_caractere_final; i >= 0; i--) {
         printf("%c", string[i]);
     }
@@ -61,10 +69,13 @@ int main() {
 int main() {
     char string[11];
     char string_2[11];
+    
     printf("Digite uma string de ate 10 caracteres : \n");
     gets(string);
+    
     printf("Digite uma string para verificarmos se esta contida na string digitada anteriormente: \n");
     gets(string_2);
+    
     if (strstr(string, string_2) != NULL) {
         printf("A string %s esta contida na string %s", string_2, string);
     } else {
@@ -72,6 +83,7 @@ int main() {
     }
   return 0;
 }
+
 //QUESTAO 5:
 
 include <stdio.h>
@@ -83,15 +95,19 @@ int main() {
     int i;
     int quantidade_vogais = 0;
     char caractere_para_substituir_vogais;
+    
     printf("Digite uma string de ate 10 caracteres : \n");
     gets(string);
+    
     for (i = 0; i < strlen(string); i++) {
         if (string[i] == 'a' || string[i] == 'e' || string[i] == 'i' || string[i] == 'o' || string[i] == 'u') {
             quantidade_vogais++;
         }
     }
+    
     printf("Digite um caractere para substituir todas as vogais da string anterior: \n");
     scanf(" %c", &caractere_para_substituir_vogais);
+    
     for (i = 0; i < strlen(string); i++) {
         if (string[i] == 'a' || string[i] == 'e' || string[i] == 'i' || string[i] == 'o' || string[i] == 'u') {
             string[i] = caractere_para_substituir_vogais;
