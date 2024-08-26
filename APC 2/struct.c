3044e11104cca773b5d77f35afc6f0cc17f09922
Alanna Tomaz <160501486+alannatomaz@users.noreply.github.com>
2024-08-26 13:52:15 +0000

    finalizar parte de struct

--- APC 2/struct.c
+++ APC 2/struct.c
@@ -20,19 +20,24 @@ int main() {
 
   struct Pessoa pessoa2;
 
+  // printf("\nEntre com NOME: ");
+  // scanf("%s", pessoa2.nome);  //Nao precisa do & ja que se refere a um vetor (e vet eh pont.)
+
   printf("\nEntre com NOME: ");
-  scanf("%s", &pessoa2.nome);
+  scanf("%99[^\n]", pessoa2.nome); //Ler, nao %s, mas um %TUDO MENOS \n
 
   printf("Entre com IDADE: ");
-  scanf("%d", &pessoa2.idade);
+  scanf("%d", &pessoa2.idade); //scanf("%d%*c", &pessoa2.idade);
 
   printf("Entre com GENERO: ");
-  scanf(" %c", &pessoa2.genero);
+  scanf(" %c", &pessoa2.genero); 
 
   printf("\nNome: %s\n", pessoa2.nome);
   printf("Idade: %d\n", pessoa2.idade);
   printf("Gênero: %c\n", pessoa2.genero);
 
+
+  //Faltou pegar direito a parte de "Acessando atributos de estrutura com ponteiros e setas"
 
   return 0;
 }
